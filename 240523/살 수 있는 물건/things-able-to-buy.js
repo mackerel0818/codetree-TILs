945c08cf4e9ec940889d48235e11a410279bdde3
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-23 20:13:03 +0900

    Title: 살 수 있는 물건, Time: 234ms, Memory: 14MB, Status: Passed - CodeTree

--- 240523/살 수 있는 물건/things-able-to-buy.js
+++ 240523/살 수 있는 물건/things-able-to-buy.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-if(1000<=n<3000){
-    console.log('mask')
-}else if(3000<=n){
-    console.log('book')
-} else{
-    console.log('no')
+if (n < 1000) {
+  console.log('no')
+} else if (n < 3000) {
+  console.log('mask')
+} else {
+  console.log('book')
 }
