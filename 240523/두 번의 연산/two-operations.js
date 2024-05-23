03c725ece09d355833a5abc3fbaaf6cf2ed5dbe1
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-23 20:24:53 +0900

    Title: 두 번의 연산, Time: 211ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240523/두 번의 연산/two-operations.js
+++ 240523/두 번의 연산/two-operations.js
@@ -4,7 +4,9 @@ let a = Number(fs.readFileSync(0).toString().trim());
 
 if(a%2 === 1){
     a+=3
-    if(a%3===0){
-        console.log(a/3)
-    }
+    
+} 
+
+if(a%3===0){
+    console.log(a/3)
 }
