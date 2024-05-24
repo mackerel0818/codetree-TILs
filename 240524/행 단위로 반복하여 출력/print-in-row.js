44654757a77b5297904d1a5ecd28b15b03f699ef
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-24 21:20:30 +0900

    Title: 행 단위로 반복하여 출력, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240524/행 단위로 반복하여 출력/print-in-row.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString());
+let str = ''
+
+for (let i = 0; i < n; i++) {
+    str = "";
+    for (let j = 0; j < n; j++) {
+        str += j + 1;
+    }
+    console.log(str);
+}
