ec87b9ba666406e79994d5e1b52e99e050e2624c
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-24 12:30:41 +0900

    Title: 세 수의 중간값, Time: 184ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240524/세 수의 중간값/median-of-three-numbers.js
@@ -0,0 +1,12 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0])
+let b = Number(input[1])
+let c = Number(input[2])
+
+if (b >a && c>b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
