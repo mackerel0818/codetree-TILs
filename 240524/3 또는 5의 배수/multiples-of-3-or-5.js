98bbd5d1e47587064bfd3c0fc19d348bf2c5e4b5
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-24 12:21:09 +0900

    Title: 3 또는 5의 배수, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240524/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = Number(fs.readFileSync(0).toString());
+
+if (input % 3 == 0) {
+    console.log('YES');
+}
+else {
+    console.log('NO');
+}
+
+if(input % 5 == 0) {
+    console.log('YES')
+}
+else {
+    console.log('NO')
+}
