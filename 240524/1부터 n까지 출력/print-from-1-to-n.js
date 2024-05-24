c62104bf911a8b472c887ddd145124f49343e995
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-24 19:39:25 +0900

    Title: 1부터 n까지 출력, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240524/1부터 n까지 출력/print-from-1-to-n.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim());
+let list = []
+
+for (let i=0; i<input;i++) {
+    list[i] = i+1
+}
+
+console.log(list[0], list[1], list[2])
