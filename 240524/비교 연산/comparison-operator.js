b0f71bd2098833ebd2d97170f49c1eae722f2978
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-24 12:23:08 +0900

    Title: 비교 연산, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240524/비교 연산/comparison-operator.js
@@ -0,0 +1,46 @@
+let fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0])
+let b = Number(input[1])
+
+if (a>=b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
+
+if (a>b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
+
+if (a<=b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
+
+if (a<b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
+
+if (a==b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
+
+if (a!=b) {
+    console.log(1);
+}
+else {
+    console.log(0);
+}
