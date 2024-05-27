96feb15889d0f853c576b2238c241286a9c1a41d
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-05-27 09:17:38 +0900

    Title: 문자열 출력하기, Time: 188ms, Memory: 14MB, Status: Passed - CodeTree

--- 240527/문자열 출력하기/print-string.js
+++ 240527/문자열 출력하기/print-string.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
-let input = Number(fs.readFileSync(0).toString())
+let input = Number(fs.readFileSync(0).toString());
 
 for (let i = 0; i < input; i++) {
-    console.log("LeebosCode")
+    console.log("LeebrosCode")
 }
