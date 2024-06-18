c6205e361774931a49fc9e35e8c3378246828882
mackerel0818 <78207698+mackerel0818@users.noreply.github.com>
2024-06-18 17:37:41 +0900

    Title: 문자 출력하기, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240618/문자 출력하기/print-text.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+let input = fs.readFileSync(0).toString().trim();
+let list = [];
+
+for (let i = 1; i <= 8; i++) {
+    list.push(input);
+}
+
+console.log(list.join(""))
